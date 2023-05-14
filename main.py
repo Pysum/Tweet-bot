0ac4744b05cec3cc00a3d68283b4bdb3247cdc66
Sumit <126980669+Pysum@users.noreply.github.com>
2023-05-14 21:08:01 +0530

    main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-TWITTER_USERNAME = "@pysumM"
-TWITTER_PASSWORD = "Sumit1988@#"
-chrome_driver = "chromedriver.exe"
+TWITTER_USERNAME = "YOUR USERNAME"
+TWITTER_PASSWORD = "YOUR PASSWORD"
+chrome_driver = "CHROME DRIVER PATH"
 servie = Service(chrome_driver)
 driver = webdriver.Chrome(service=servie)
 
